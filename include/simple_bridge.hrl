12aec89a51b6129d3386993bc0b8fb2c5ca64a05
Maxim Sokhatsky <maxim@synrc.com>
2013-04-29 10:27:40 +0300

    remove simple_bridge include copy

--- include/simple_bridge.hrl
+++
@@ -1,9 +0,0 @@
--ifndef(debug_print).
--define(debug_print, true).
--define(PRINT(Var), error_logger:info_msg("DEBUG: ~p:~p~n~p~n  ~p~n", [?MODULE, ?LINE, ??Var, Var])).
--endif.
-
--record(cookie, { name, value, path="/", minutes_to_live=20 }).
--record(header, { name, value }).
--record(response, { statuscode=200, headers=[], cookies=[], data=[] }).
--record(uploaded_file, { original_name, temp_file, size }).
