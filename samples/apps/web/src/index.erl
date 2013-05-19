65b881a0b8c04f386a31d84f14adfa2d3a6a5cd5
Maxim Sokhatsky <maxim@synrc.com>
2013-05-19 22:33:23 +0300

    login example

--- samples/apps/web/src/index.erl
+++ samples/apps/web/src/index.erl
@@ -3,11 +3,14 @@
 -include_lib("n2o/include/wf.hrl").
 
 main() -> 
+    case wf:user() of
+         undefined -> timer:sleep(500), wf:redirect("login");
+         _ -> 
 %    Title = "Title",
 %    Body = "Body",
     Title = wf_render_elements:render_elements(title()),
     Body = wf_render_elements:render_elements(body()),
-    [ #dtl{file = "index", bindings=[{title,Title},{body,Body}]} ].
+    [ #dtl{file = "index", bindings=[{title,Title},{body,Body}]} ] end.
 
 title() -> [ <<"N2O">> ].
 
@@ -16,7 +19,7 @@ body() -> %% area of http handler
     wf:wire(#api{name=apiOne,tag=d1}),
   [
     #span { text= <<"Your chatroom name: ">> }, 
-    #textbox { id=userName, text= <<"Anonymous">> },
+    #textbox { id=userName, text= <<"Anonymous">> }, #button{text="Logout",postback=logout}, #br{},
     #panel { id=chatHistory },
     #button{id=but,text= <<"Click Me!">>,postback=change_me},
     #button{id=replace,text= <<"Replace Body">>,postback=replace},
@@ -28,9 +31,12 @@ body() -> %% area of http handler
 
 api_event(Name,Tag,Term) -> error_logger:info_msg("Name ~p, Tag ~p, Term ~p",[Name,Tag,Term]), event(change_me).
 
+
 event(init) ->
+  User = wf:user(),
+   error_logger:info_msg("User: ~p",[User]),
   [ begin
-          Terms = [ #span { text="System" }, ": ",
+          Terms = [ #span { text= User }, ": ",
                       #span { text=integer_to_list(N) }, #br{} ],
             wf:insert_bottom(chatHistory, Terms)
             end || N <- lists:seq(1,3) ];
@@ -46,6 +52,8 @@ event(change_me) ->
 
 event(replace) ->
     wf:wire(#redirect{url="hello",nodrop=false});
+
+event(logout) -> wf:user(undefined), wf:redirect("login");
 
 event({chat,Pid}) -> %% area of websocket handler
     error_logger:info_msg("Chat Pid: ~p",[Pid]),
