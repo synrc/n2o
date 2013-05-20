d85d787e6176d11cd1328cef6a2018eb1f915c75
Maxim Sokhatsky <maxim@synrc.com>
2013-05-20 14:10:43 +0300

    disable DTL cache

--- src/elements/layout/element_dtl.erl
+++ src/elements/layout/element_dtl.erl
@@ -7,9 +7,9 @@ reflect() -> record_info(fields, dtl).
 
 render_element(Record) ->
     ModName = list_to_atom(Record#dtl.file ++ "_view"),
-    M = case code:ensure_loaded(ModName) of
-            {module,Module} -> Module;
-             _ -> erlydtl:compile(code:lib_dir(Record#dtl.app) ++ "/" ++ Record#dtl.folder 
-                                  ++ "/" ++ Record#dtl.file ++ ".html",ModName), ModName end,
-     {ok,R} = M:render(Record#dtl.bindings ++ [{script,wf_context:script()}]),
+%    M = case code:ensure_loaded(ModName) of {module,Module} -> Module; _ -> 
+    erlydtl:compile(code:lib_dir(Record#dtl.app) ++ "/" ++ Record#dtl.folder ++ "/" ++ Record#dtl.file ++ ".html",ModName), ModName 
+%    end
+    ,
+    {ok,R} = M:render(Record#dtl.bindings ++ [{script,wf_context:script()}]),
     R.
