79e167425e3c87572b586d1ebd0066111c0509ef
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-07-08 21:02:30 +0000

    fix data-* in select

--- src/elements/html/element_select.erl
+++ src/elements/html/element_select.erl
@@ -12,17 +12,16 @@ render_element(Record = #select{}) ->
                                         postback=Postback,
                                         source=[list_to_atom(ID)],
                                         delegate=Record#select.delegate }) end,
-
-  wf_tags:emit_tag(<<"select">>, wf:render(Record#select.body), [
+  Props = [
     {<<"id">>, Record#select.id},
     {<<"class">>, Record#select.class},
     {<<"style">>, Record#select.style},
     {<<"name">>, Record#select.name},
     {<<"title">>, Record#select.title},
-    {<<"data_fields">>, Record#select.data_fields},
     {<<"disabled">>, case Record#select.disabled of true -> <<"disabled">>; _-> undefined end},
     {<<"multiple">>, case Record#select.multiple of true -> <<"multiple">>; _-> undefined end} | Record#select.data_fields
-  ]);
+  ],
+  wf_tags:emit_tag(<<"select">>, wf:render(Record#select.body), Props);
 render_element(Group = #optgroup{}) ->
   wf_tags:emit_tag(<<"optgroup">>, wf:render(Group#optgroup.body), [
     {<<"disabled">>, case Group#optgroup.disabled of true-> <<"disabled">>; _-> undefined end},
