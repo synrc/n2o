ccef2f33344de3c1b44d26792ce4fe930201291a
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-07-15 16:23:51 +0000

    return name attr

--- include/wf.hrl
+++ include/wf.hrl
@@ -81,7 +81,7 @@
 -record(inplace_textbox, {?ELEMENT_BASE(element_inplace_textbox), tag, start_mode=view, validators=[], delegate=undefined}).
 -record(inplace, {?ELEMENT_BASE(element_inplace), tag, delegate=undefined, view, edit, start_mode=view}).
 
--record(upload, {?ELEMENT_BASE(element_upload), delegate}).
+-record(upload, {?ELEMENT_BASE(element_upload), name, delegate}).
 -record(wizard, {?ELEMENT_BASE(element_wizard), tag, titles, steps, next="Next", back="Back", finish="Finish",show_progress=true,progress_step="Step ",progress_of=" of "}).
 -record(sparkline, {?ELEMENT_BASE(element_sparkline), type, values, options }).
 -record(textbox_autocomplete, {?ELEMENT_BASE(element_textbox_autocomplete), tag, minLength=2, delay=300, next, postback, delegate=undefined }).
