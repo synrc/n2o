7aa84ef448bdc44f8b729b808b103e5c840de22f
Maxim Sokhatsky <maxim@synrc.com>
2013-07-18 17:03:39 +0300

    mkdir fix

--- samples/nitrogen_static.sh
+++ samples/nitrogen_static.sh
@@ -2,6 +2,7 @@
 
 HAPPYXEN=apps/n2o_sample/priv
 FILES=deps/n2o_sample/priv/static/nitrogen
+mkdir -p apps/n2o_sample
 rm -rf $FILES
 rm -rf $HAPPYXEN
 ln -s ../../../../deps/n2o/priv/static/n2o $FILES
