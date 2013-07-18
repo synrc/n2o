fefda3923b7f28e568b253ff3751da128735bf28
Maxim Sokhatsky <maxim@synrc.com>
2013-07-18 16:40:52 +0300

    make Xen happy with app stuff

--- samples/nitrogen_static.sh
+++ samples/nitrogen_static.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+HAPPYXEN=apps/n2o_sample/priv
 FILES=deps/n2o_sample/priv/static/nitrogen
 rm -rf $FILES
+rm -rf $HAPPYXEN
 ln -s ../../../../deps/n2o/priv/static/n2o $FILES
+ln -s ../../deps/n2o_sample/priv $HAPPYXEN
