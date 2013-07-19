379d267dc8c3113ef23c293f2ff7612927fcccbf
Maxim Sokhatsky <maxim@synrc.com>
2013-07-19 05:13:42 +0300

    n2o scripts bundle

--- samples/nitrogen_static.sh
+++ samples/nitrogen_static.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-HAPPYXEN=apps/n2o_sample/priv
-FILES=deps/n2o_sample/priv/static/nitrogen
+N2O=deps/n2o/priv/static
+APP=deps/n2o_sample/priv/static/nitrogen
+XEN=apps/n2o_sample/priv
+rm -rf $N2O
+rm -rf $APP
+rm -rf $XEN
 mkdir -p apps/n2o_sample
-rm -rf $FILES
-rm -rf $HAPPYXEN
-ln -s ../../../../deps/n2o/priv/static/n2o $FILES
-ln -s ../../deps/n2o_sample/priv $HAPPYXEN
+ln -s ../../n2o_scripts $N2O
+ln -s ../../../../deps/n2o/priv/static/n2o $APP
+ln -s ../../deps/n2o_sample/priv $XEN
