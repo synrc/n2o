4ce9bd8c2659032b0ae845d8ce676a068ee7a5e8
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-07-30 12:39:53 +0000

    clickable tr

--- src/elements/element_tr.erl
+++ src/elements/element_tr.erl
@@ -2,9 +2,13 @@
 -include("wf.hrl").
 -compile(export_all).
 
-render_element(Record) ->
+render_element(Record = #tr{postback= Postback}) ->
+  Id = Record#tr.id,
+  Cursor = case Postback of undefined -> "";
+    P -> wf:wire(#event {type=click, postback=P, validation_group=Id, delegate=Record#tr.delegate}), "cursor:pointer;"
+  end,
   wf_tags:emit_tag(<<"tr">>, wf:render(Record#tr.cells), [
     {<<"id">>, Record#tr.id},
     {<<"class">>, Record#tr.class},
-    {<<"style">>, Record#tr.style}
+    {<<"style">>, [Record#tr.style, Cursor]}
   ]).
