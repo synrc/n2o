327085acddc91265db22b4e807dcb156fecab27e
Andrii Zadorozhnii <andrii.zadorozhnii@gmail.com>
2013-08-31 15:20:26 +0300

    correct checked state

--- src/elements/element_checkbox.erl
+++ src/elements/element_checkbox.erl
@@ -7,10 +7,6 @@ reflect() -> record_info(fields, checkbox).
 
 render_element(Record) -> 
     Id = Record#checkbox.id,
-    CheckedOrNot = case Record#checkbox.checked of
-        true -> <<"checked">>;
-        _ -> <<"not_checked">>
-    end,
     case Record#checkbox.postback of
         undefined -> ignore;
         Postback -> wf:wire(Id, #event { type=change, postback=Postback, validation_group=Id, delegate=Record#checkbox.delegate })
@@ -23,7 +19,7 @@ render_element(Record) ->
             {<<"type">>, <<"checkbox">>},
             {<<"data-toggle">>, <<"checkbox">>},
             {<<"value">>, Record#checkbox.value},
-            {CheckedOrNot, true}
+            {<<"checked">>, if Record#checkbox.checked==true -> <<"checked">>; true -> undefined end}
       ]),
       Record#checkbox.body
     ],
