7d21833963853e033288837e7cd179306df9b4a1
Maxim Sokhatsky <maxim@synrc.com>
2013-10-04 23:40:03 +0300

    compact redirect

--- src/actions/action_redirect.erl
+++ src/actions/action_redirect.erl
@@ -2,15 +2,7 @@
 -include_lib("n2o/include/wf.hrl").
 -compile(export_all).
 
-render_action(Record=#redirect{nodrop=false}) ->
+render_action(Record=#redirect{}) ->
     DestinationUrl = Record#redirect.url,
     wf:f("window.location=\"~s\";", [wf:js_escape(DestinationUrl)]);
-
-render_action(Record=#redirect{nodrop=true}) ->
-    Html = wf_core:render_item(#dtl{file=Record#redirect.url,bindings=[{hello,"Hello"}]}),
-    Re = re:replace(lists:flatten(Html),"\n"," ",[global,{return,list}]),
-    error_logger:info_msg("Html: ~p",[Re]),
-    wf:f("$('body').html('~s');", [Re]).
-
-redirect(Url) -> wf:wire(#redirect { url=Url }).
 
