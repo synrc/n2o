cf514410d8727d8ff901559db65f1d0d5f8429d3
Maxim Sokhatsky <maxim@synrc.com>
2013-10-07 02:08:35 +0300

    ripemd160

--- src/handlers/n2o_secret.erl
+++ src/handlers/n2o_secret.erl
@@ -8,7 +8,7 @@ pickle(Data) ->
     Padding = size(Message) rem 16,
     Bits = (16-Padding)*8, Key = secret(), IV = crypto:rand_bytes(16),
     Cipher = crypto:block_encrypt(aes_cbc128,Key,IV,<<Message/binary,0:Bits>>),
-    Signature = crypto:md5(<<Key/binary,Cipher/binary>>),
+    Signature = crypto:hash(ripemd160,<<Key/binary,Cipher/binary>>),
     base64:encode(<<IV/binary,Signature/binary,Cipher/binary>>).
 
 secret() -> wf:config(n2o,secret,<<"ThisIsClassified">>).
@@ -16,8 +16,8 @@ secret() -> wf:config(n2o,secret,<<"ThisIsClassified">>).
 depickle(PickledData) ->
     try Key = secret(),
         Decoded = base64:decode(wf:to_binary(PickledData)),
-        <<IV:16/binary,Signature:16/binary,Cipher/binary>> = Decoded,
-        Signature = crypto:md5(<<Key/binary,Cipher/binary>>),
+        <<IV:16/binary,Signature:20/binary,Cipher/binary>> = Decoded,
+        Signature = crypto:hash(ripemd160,<<Key/binary,Cipher/binary>>),
         {Data,_Time} = binary_to_term(crypto:block_decrypt(aes_cbc128,Key,IV,Cipher)),
         Data
     catch _:_ -> undefined end.
