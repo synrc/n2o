a77143a2b994e197594a2f83a9996a7089b7663e
Maxim Sokhatsky <maxim@synrc.com>
2013-10-06 08:04:55 +0300

    fix redirect

--- src/actions/action_jq.erl
+++ src/actions/action_jq.erl
@@ -9,5 +9,9 @@ render_action(Record=#jq{property=undefined,target=Target,method=Methods}) ->
 render_action(#jq{target=Target,method=undefined,property=Property,right=undefined}) ->
     wf:f("$('#~s').~s;", [Target,Property]);
 
+render_action(#jq{target=Target,method=undefined,property=Property,args=simple,right=Right}) ->
+    wf:f("~s.~s = ~s", [Target,Property,wf:render(Right)]);
+
 render_action(#jq{target=Target,method=undefined,property=Property,right=Right}) ->
-    wf:f("~s.~s = ~s", [Target,Property,wf:render(Right)]).
+    wf:f("$('#~s').~s = ~s", [Target,Property,wf:render(Right)]).
+
--- src/wf.erl
+++ src/wf.erl
@@ -42,7 +42,7 @@ flush(Key) -> action_async:flush(Key).
 % Redirect and purge connection wf:redirect
 
 redirect(Url) ->
-    wf:wire(#jq{target=window,property=location,right=["\"",Url,"\""]}).
+    wf:wire(#jq{target=window,property=location,args=simple,right=["\"",Url,"\""]}).
 
 % Message Bus communications wf:reg wf:send
 
