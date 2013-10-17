9f6549ac41d4b87df7972839282c44fc55b54873
Maxim Sokhatsky <maxim@synrc.com>
2013-10-17 20:59:59 +0300

    fix args

--- src/actions/action_jq.erl
+++ src/actions/action_jq.erl
@@ -3,13 +3,11 @@
 -compile(export_all).
 
 render_action(Record=#jq{property=undefined,target=Target,method=Methods}) ->
-    Arg = string:join([wf:to_list(wf:render(A))||A<-Record#jq.args],""),
-    string:join([wf:f("$('#~s').~s('~s');", [Target,Method,Arg]) || Method <- Methods],"");
+    Arg = string:join(["\""++wf:to_list(wf:render(A))++"\""||A<-Record#jq.args],","),
+    string:join([wf:f("$('#~s').~s(~s);", [Target,Method,Arg]) || Method <- Methods],"");
 
 render_action(#jq{target=Target,method=undefined,property=Property,args=simple,right=Right}) ->
-    X = wf:f("~s.~s = ~s;", [Target,Property,Right]),
-    wf:info("simple redirect ~p",[X]),
-    X;
+    wf:f("~s.~s = ~s;", [Target,Property,Right]);
 
 render_action(#jq{target=Target,method=undefined,property=Property,right=undefined}) ->
     wf:f("$('#~s').~s;", [Target,Property]);
