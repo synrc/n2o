dfc0a469bb8360505469420c8fe4bb29fb536abf
Namdak Tonpa <maxim@synrc.com>
2013-12-04 10:55:37 +0200

    Update wf.hrl

--- include/wf.hrl
+++ include/wf.hrl
@@ -129,9 +129,7 @@
 
 % REST macros
 -define(rest(), is_rest() -> true).
-
 -define(unmap(Record), unmap(P,R) -> wf_utils:hunmap(P,R,record_info(fields, Record),size(R)-1)).
-
 -define(map(Record), map(O) ->
     Y = [ try N=lists:nth(1,B), if is_number(N) -> wf:to_binary(B); true -> B end catch _:_ -> B end
           || B <- tl(tuple_to_list(O)) ],
