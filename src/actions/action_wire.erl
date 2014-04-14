ad96ffeda0ceace72068e8a6084d16bb27db96dc
Maxim Sokhatsky <maxim@synrc.com>
2014-04-14 13:44:48 +0300

    action_wire back

---
+++ src/actions/action_wire.erl
@@ -0,0 +1,10 @@
+-module(action_wire).
+-author('Maxim Sokhatsky').
+-include_lib("n2o/include/wf.hrl").
+-compile(export_all).
+
+render_action(#wire{actions=Actions}) -> wf:render(Actions);
+render_action(S) when is_list(S) -> S;
+render_action(_) -> [].
+
+wire(Actions) -> wf_context:add_action(#wire{actions=Actions}).
