515fb0e403a8c469a3a9fa309bdc8ce06239dfcd
doxtop <andrii.zadorozhnii@gmail.com>
2014-04-17 01:17:13 +0300

    return action_api

---
+++ src/actions/action_api.erl
@@ -0,0 +1,11 @@
+-module(action_api).
+-author('Maxim Sokhatsky').
+-include_lib("n2o/include/wf.hrl").
+-compile(export_all).
+
+render_action(Record) ->
+    Name = Record#api.name,
+    Data = "utf8.toByteArray(JSON.stringify(data))",
+    PostbackScript = wf_event:new(Name, "document", Record#api.delegate, api_event, Data),
+    wf:f("~s = function(data) {",  [Name]) ++ PostbackScript ++ "};".
+
