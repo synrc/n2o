17f2873a1e3c0e653b973d76928a20f2da4b1f42
doxtop <andrii.zadorozhnii@gmail.com>
2014-04-17 00:58:26 +0300

    merge actions

--- src/actions/action_event.erl
+++ src/actions/action_event.erl
@@ -9,9 +9,8 @@ render_action(#event{postback=Postback,actions=Actions,source=Source,target=Cont
         {Key, Id} = if  is_atom(Src)-> S = atom_to_list(Src),
                         {"Bert.atom('"++S++"')", S};
                     true -> {"utf8.toByteArray('" ++ Src ++ "')", Src} end,
-        "Bert.tuple(" ++ Key ++ ", utf8.toByteArray(document.querySelector('#" 
-                      ++ Id ++ "').value))" end
-    || Src <- Source ],",") ++ "]",
-    PostbackScript = wf_event:new(Postback, Control, Delegate, event, Data),
-    [ wf:f("document.querySelector('#~s').addEventListener('~s',function (event) { ",
-        [Control,Type]),PostbackScript,"});"].
+        "Bert.tuple(" ++ Key ++ ", utf8.toByteArray(document.querySelector('#"
+                      ++ Id ++ "').value))" end || Src <- Source] 
+    ++ ["Bert.tuple(Bert.tuple(utf8.toByteArray('"++ Control ++"'), Bert.binary('detail')), event.detail)"],",") ++ "]",
+    PostbackBin = wf_event:new(Postback, Control, Delegate, event, Data),
+    [wf:f("document.querySelector('#~s').addEventListener('~s',function (event){", [Control,Type]),PostbackBin,"});"].
