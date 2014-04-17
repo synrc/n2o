fd8dc9d40592ad3600abefa755e52c280609c965
doxtop <andrii.zadorozhnii@gmail.com>
2014-04-17 20:48:39 +0300

    delegate to any element. fix reupload and thumbnails

--- include/wf.hrl
+++ include/wf.hrl
@@ -19,38 +19,38 @@
 -define(DEFAULT_BASE_TAG(Tag), {?ELEMENT_BASE_TAG(undefined,Tag)}).
 -define(ELEMENT_BASE(Module), ?ELEMENT_BASE_TAG(Module,undefined)).
 -define(ELEMENT_BASE_TAG(Module,Tag),
-        ancestor=element, module=Module, id, actions, class=[], style=[], source=[],
+        ancestor=element, module=Module, delegate=Module, id, actions, class=[], style=[], source=[],
         data_fields=[], aria_states=[], body, role, tabindex, show_if=true, html_tag=Tag, title).
 
 -define(ACTION_BASE(Module),
         ancestor=action, trigger, target, module=Module, actions, source=[]).
 
 -record(element, {?ELEMENT_BASE(undefined)}).
--record(range, {?ELEMENT_BASE(element_range), min=0, max=100, step=1, value=0, next, postback, delegate}).
+-record(range, {?ELEMENT_BASE(element_range), min=0, max=100, step=1, value=0, next, postback}).
 -record(literal, {?ELEMENT_BASE(element_literal)}).
 -record(dtl, {?ELEMENT_BASE(element_dtl), file="index", bindings=[], app=web, folder="priv/templates", ext="html", bind_script=true }).
 -record(list, {?ELEMENT_BASE(element_list), numbered=false }).
 -record(label, {?ELEMENT_BASE(element_label), for=""}).
--record(link, {?ELEMENT_BASE(element_link), target, url="javascript:void(0);", postback, delegate, name, download}).
--record(submit, {?ELEMENT_BASE(element_submit), click, postback, delegate}).
--record(button, {?ELEMENT_BASE(element_button), type= <<"button">>, name, value, postback, delegate, disabled}).
--record(textbox, {?ELEMENT_BASE(element_textbox), value, disabled, maxlength="", placeholder="", next, postback, delegate, name}).
+-record(link, {?ELEMENT_BASE(element_link), target, url="javascript:void(0);", postback, name, download}).
+-record(submit, {?ELEMENT_BASE(element_submit), click, postback}).
+-record(button, {?ELEMENT_BASE(element_button), type= <<"button">>, name, value, postback, disabled}).
+-record(textbox, {?ELEMENT_BASE(element_textbox), value, disabled, maxlength="", placeholder="", next, postback, name}).
 -record(hidden, {?ELEMENT_BASE(element_hidden), value, html_name, disabled=false}).
 -record(textarea, {?ELEMENT_BASE(element_textarea), placeholder, name, cols, rows, value}).
 -record(optgroup, {?ELEMENT_BASE(element_select), label, disabled}).
 -record(option, {?ELEMENT_BASE(element_select), label, value, selected=false, disabled}).
--record(dropdown, {?ELEMENT_BASE(element_dropdown), options, postback, delegate, value, multiple=false, disabled=false, name}).
--record(select, {?ELEMENT_BASE(element_select), disabled, multiple, name, size, postback, delegate}).
--record(checkbox, {?ELEMENT_BASE(element_checkbox), checked=false, value="on", postback, delegate, name}).
+-record(dropdown, {?ELEMENT_BASE(element_dropdown), options, postback, value, multiple=false, disabled=false, name}).
+-record(select, {?ELEMENT_BASE(element_select), disabled, multiple, name, size, postback}).
+-record(checkbox, {?ELEMENT_BASE(element_checkbox), checked=false, value="on", postback, name}).
 -record(radiogroup, {?ELEMENT_BASE(element_radiogroup)}).
--record(radio, {?ELEMENT_BASE(element_radio), value, name, checked=false, postback, delegate, html_name}).
--record(password, {?ELEMENT_BASE(element_password), value, maxlength="", placeholder= <<"password">>, next, postback, delegate, name}).
+-record(radio, {?ELEMENT_BASE(element_radio), value, name, checked=false, postback, html_name}).
+-record(password, {?ELEMENT_BASE(element_password), value, maxlength="", placeholder= <<"password">>, next, postback, name}).
 -record(spinner, {?ELEMENT_BASE(element_spinner), image="/nitrogen/spinner.gif"}).
 -record(image, {?ELEMENT_BASE(element_image), image="", alt="", width, height}).
 -record(table, {?ELEMENT_BASE(element_table), header, footer}).
 -record(td, {?ELEMENT_BASE(element_td), colspan=1, rowspan=1, scope}).
 -record(th, {?ELEMENT_BASE(element_th), colspan=1, rowspan=1, scope}).
--record(tr, {?ELEMENT_BASE(element_tr), cells, postback, delegate}).
+-record(tr, {?ELEMENT_BASE(element_tr), cells, postback}).
 -record(file, {?ELEMENT_BASE(element_file), file}).
 -record(blockquote, {?ELEMENT_BASE(element_blockquote), cite}).
 
@@ -114,18 +114,17 @@
 -record(slider, {?ELEMENT_BASE(element_slider), min, max, step, orientation, value, selection, tooltip, handle, formater}).
 
 % Synrc Elements
--record(rtable, {?ELEMENT_BASE(element_rtable), rows=[], delegate, postback}).
--record(upload_state, {cid, delegate, root=code:priv_dir(n2o), dir="", name, type, room=upload, data= <<>>, preview=false, size=[{200,200}]}).
+-record(rtable, {?ELEMENT_BASE(element_rtable), rows=[], postback}).
+-record(upload_state, {cid, root=code:priv_dir(n2o), dir="", name, type, room=upload, data= <<>>, preview=false, size=[{200,200}], index=0}).
 -record(upload, {?ELEMENT_BASE(element_upload), name, value, state=#upload_state{}}).
--record(textboxlist, {?ELEMENT_BASE(element_textboxlist), placeholder="", delegate, postback, unique=true, values=[], autocomplete=true, queryRemote=true, onlyFromValues=true, minLenght=1}).
--record(htmlbox, {?ELEMENT_BASE(wf:config(n2o,htmlbox_module,element_htmlbox)), html, script_url="static/tinymce/tinymce.min.js", theme="n2o", delegate, delegate_api, toolbar_class, toolbar_script, root=code:priv_dir(n2o), dir="", post_write, img_tool, post_target, size=[{200, 200}]}).
+-record(textboxlist, {?ELEMENT_BASE(element_textboxlist), placeholder="", postback, unique=true, values=[], autocomplete=true, queryRemote=true, onlyFromValues=true, minLenght=1}).
+-record(htmlbox, {?ELEMENT_BASE(wf:config(n2o,htmlbox_module,element_htmlbox)), html, script_url="static/tinymce/tinymce.min.js", theme="n2o", delegate_api, toolbar_class, toolbar_script, root=code:priv_dir(n2o), dir="", post_write, img_tool, post_target, size=[{200, 200}]}).
 
 % Actions
 -record(action,  {?ACTION_BASE(undefined)}).
 -record(wire,    {?ACTION_BASE(action_wire)}).
 -record(api,     {?ACTION_BASE(action_api), name, tag, delegate }).
 -record(event,   {?ACTION_BASE(action_event), type=default, postback, delegate}).
--record(control, {?ACTION_BASE(action_control), type=default, delegate}).
 -record(alert,   {?ACTION_BASE(action_alert), text}).
 -record(confirm, {?ACTION_BASE(action_confirm), text, postback, delegate}).
 -record(jq,      {?ACTION_BASE(action_jq), property, method, args=[], right, format="~s"}).
--- src/elements/element_upload.erl
+++ src/elements/element_upload.erl
@@ -6,17 +6,26 @@
 -compile(export_all).
 
 -ifndef(IMG_TOOL).
--define(IMG_TOOL, wf:config(n2o, img_tool, ?MODULE)).
+-define(IMG_TOOL, (wf:config(n2o, img_tool, ?MODULE))).
+-endif.
+-ifndef(TH_DIR).
+-define(TH_DIR, (wf:config(n2o, thumbnail_dir, "thumbnails"))).
 -endif.
 
 -define(WS_SEND(Id,Ev,Detail), wf:wire(wf:f("document.getElementById('~s').dispatchEvent("
   "new CustomEvent('~s', {'detail': ~s}))", [Id,wf:to_list(Ev),wf:json([Detail])]))).
 
-init([#upload_state{root=Root, dir=Dir, name=Name}=S]) ->
-  case file:open(filename:join([Root, Dir, Name]), [raw, binary, write]) of
-    {ok, D} -> file:close(D),{ok,S};{error, enoent}->{ok,S};{error,enotdir}->{ok,S};{error,R}->{stop,R} end.
-
-handle_call({deliver_slice, Data}, _, #upload_state{data=Old}=S) ->
+init([#upload_state{root=Root, dir=Dir, name=Name, index=I}=S]) ->
+  case file:open(filename:join([Root, Dir, Name]), [raw, binary, write, read]) of {ok, D} ->
+    ReadS =  if S#upload_state.index == 0 -> {ok, S};
+      true -> case file:read(D, I) of {ok, Dl} -> {ok, S#upload_state{data=Dl}};_ -> {ok, S} end end,
+    file:close(D),
+    wf:reg(S#upload_state.room),
+    ReadS;
+  {error, enoent}->{ok,S};
+  {error,enotdir}->{ok,S};
+  {error,R}->{stop,R} end.
+handle_call({deliver_slice, Data},_, #upload_state{data=Old}=S) ->
   New = <<Old/binary, Data/binary>>,
   {reply, {delivered, erlang:byte_size(New)}, S#upload_state{data=New}}.
 handle_cast(complete, #upload_state{}=S) -> {stop, normal, S}.
@@ -24,14 +33,23 @@ handle_info(_,S) -> {noreply, S}.
 terminate(normal, S) ->
   File = filename:join([S#upload_state.root, S#upload_state.dir, S#upload_state.name]),
   filelib:ensure_dir(File),
-  file:write_file(File, S#upload_state.data, [write, raw]),
-
-  ?WS_SEND(S#upload_state.cid, done, []), wf:flush(S#upload_state.room);
+  Ext = filename:extension(File),
+  Name = filename:basename(File, Ext),
+  ThDir = filename:join([S#upload_state.root, S#upload_state.dir,?TH_DIR]),
+  {Ev,D} = case file:write_file(File, S#upload_state.data, [write, raw]) of ok ->
+    [begin
+      ThName = filename:join([ThDir,Name++"_"++integer_to_list(W)++"x"++integer_to_list(H)++Ext]),
+      filelib:ensure_dir(ThName),
+      ?IMG_TOOL:make_thumb(File,W,H,ThName)
+     end || {W,H}<-S#upload_state.size],
+    {upload_complete, {file, list_to_binary(filename:join(S#upload_state.dir, S#upload_state.name))}};
+    {error, R} -> {error, {msg, R}} end,
+  ?WS_SEND(S#upload_state.cid, Ev, D),
+  wf:flush(S#upload_state.room);
 terminate(R,S) ->
   case R of shutdown -> ok; {shutdown, _T} -> ok;
     E -> ?WS_SEND(S#upload_state.cid, error, [{msg, E}]), wf:flush(S#upload_state.room) end.
 code_change(_,S,_) -> {ok,S}.
-
 
 render_element(#upload{id=Id, state=S} = R) ->
   Uid = case Id of undefined -> wf:temp_id(); I -> I end,
@@ -47,36 +65,36 @@ render(#upload{id=Id} = R) ->
     {<<"style">>, <<"display:none">>},
     {<<"name">>, R#upload.name} ]).
 
-wire(#upload{id=Id, state=S}=R) ->
+wire(#upload{id=Id, state=S, delegate=D}=R) ->
   Callbacks = [{query_file, [Id], S},{start_upload, [], S}, {deliver, [], S}, {complete, [], ok}],
 
   wf:wire(wf:f("Upload('#~s',{preview:'~s',value:'~s'});", [Id, wf:to_list(S#upload_state.preview), R#upload.value])),
-  [wf:wire(#event{postback={Id,T,Arg}, source=Src, target=Id, type=T, delegate=?MODULE}) || {T, Src, Arg} <- Callbacks].
+  [wf:wire(#event{postback={Id,T,Arg}, source=Src, target=Id, type=T, delegate=D}) || {T, Src, Arg} <- Callbacks].
 
-
-event({_,_,#upload_state{delegate=M}}=E) when M /=undefined -> M:event(E);
 event({Id, query_file, #upload_state{}=S}) ->
   [{file_name,Name}] = wf:q({Id, <<"detail">>}),
   Absname = filename:join([S#upload_state.root, S#upload_state.dir, wf:to_list(Name)]),
   Size = case file:read_file_info(Absname) of {ok, Fi} -> Fi#file_info.size; {error, _} -> 0 end,
   ?WS_SEND(Id, queried,{file_size, Size});
+
 event({Id, start_upload, #upload_state{}=S}) ->
-  wf:reg(S#upload_state.room),
-  [{file_name, Name}, {type, MimeType}] = wf:q({Id, <<"detail">>}),
-  Absname = filename:join([S#upload_state.root, S#upload_state.dir, wf:to_list(Name)]),
-  {Ev, Detail} = case gen_server:start(?MODULE, [S#upload_state{cid=Id, name=wf:to_list(Name), type=MimeType}], []) of
+  [{file_name, Name}, {type, MimeType}, {index, Index}] = wf:q({Id, <<"detail">>}),
+  UpS=S#upload_state{cid=Id, name=wf:to_list(Name), type=MimeType, index=Index},
+  {Ev, Detail} = case gen_server:start(?MODULE, [UpS], []) of
     {ok, Pid} -> {read_slice,  {pid, list_to_binary(pid_to_list(Pid))}};
     {error, R} when is_atom(R) -> {error, {msg, atom_to_list(R)}};
     {error, R} -> {error, {msg, R}} end,
-
   ?WS_SEND(Id, Ev, Detail);
-event({Id, deliver, #upload_state{}=S}) ->
+
+event({Id, deliver,_}) ->
   [{pid, Pid}, {data, Data}] = wf:q({Id, <<"detail">>}),
   {Ev, Size} = gen_server:call(list_to_pid(Pid), {deliver_slice, Data}),
   ?WS_SEND(Id, Ev, {file_size, Size});
+
 event({Id, complete,_}) ->
   [{pid, Pid}] = wf:q({Id, <<"detail">>}),
   gen_server:cast(list_to_pid(binary_to_list(Pid)), complete);
+
 event(R) -> skip.
 
 hash(Filename) ->
