e81e00a7423d1d10932fb6f393fb60fea5023453
Maxim Sokhatsky <maxim@synrc.com>
2014-05-02 02:27:51 +0300

    move synrc controls to n2o_bootstrap

--- include/wf.hrl
+++ include/wf.hrl
@@ -107,19 +107,6 @@
 % Embedding
 -record(canvas,     ?DEFAULT_BASE).
 
-% Twitter Bootstrap Elements
--record(carousel, {?ELEMENT_BASE(element_carousel), interval=5000, pause= <<"hover">>, start=0, indicators=true, items=[], caption=[]}).
--record(accordion, {?ELEMENT_BASE(element_accordion), items=[], nav_stacked=false}).
--record(slider, {?ELEMENT_BASE(element_slider), min, max, step, orientation, value, selection, tooltip, handle, formater}).
-
-% Synrc Elements
--record(rtable, {?ELEMENT_BASE(element_rtable), rows=[], postback}).
--record(upload_state, {cid, root=code:priv_dir(n2o), dir="", name,
-  type, room=upload, data= <<>>, preview=false, size=[{200,200}], index=0, block_size=1048576}).
--record(upload, {?CTRL_BASE(element_upload), name, value, state=#upload_state{}}).
--record(textboxlist, {?ELEMENT_BASE(element_textboxlist), placeholder="", postback, unique=true, values=[], autocomplete=true, queryRemote=true, onlyFromValues=true, minLenght=1}).
--record(htmlbox, {?CTRL_BASE(element_htmlbox), html, state=#upload_state{}}).
-
 % Actions
 -record(action,  {?ACTION_BASE(undefined)}).
 -record(wire,    {?ACTION_BASE(action_wire)}).
