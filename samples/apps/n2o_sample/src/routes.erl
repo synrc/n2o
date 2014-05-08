ef277e617db53c699806b7ddaa001200814b348b
Maxim Sokhatsky <maxim@synrc.com>
2014-05-09 00:41:16 +0300

    new otp.mk, new router

--- samples/apps/n2o_sample/src/routes.erl
+++ samples/apps/n2o_sample/src/routes.erl
@@ -7,17 +7,17 @@
 finish(State, Ctx) -> {ok, State, Ctx}.
 init(State, Ctx) -> 
     Path = wf:path(Ctx#context.req),
-    {ok, State, Ctx#context{path=Path,module=route(Path)}}.
+    {ok, State, Ctx#context{path=Path,module=route_prefix(Path)}}.
 
-route(<<"/">>) -> login;
-route(<<"/index">>) -> index;
-route(<<"/login">>) -> login;
-route(<<"/lispchat">>) -> lispchat;
-route(<<"/ws/">>) -> login;
-route(<<"/ws/lispchat">>) -> lispchat;
-route(<<"/ws/index">>) -> index;
-route(<<"/ws/login">>) -> login;
-route(<<"/ws/static/spa/spa.htm">>) -> login;
-route(<<"/ws/static/spa/index.htm">>) -> index;
-route(<<"/favicon.ico">>) -> static_file;
+route_prefix(<<"/ws/",P/binary>>) -> route(P);
+route_prefix(<<"/",P/binary>>) -> route(P);
+route_prefix(P) -> route(P).
+
+route(<<>>)              -> index;
+route(<<"index">>)       -> index;
+% route(<<"index2">>)       -> index2;
+route(<<"login">>)       -> login;
+route(<<"favicon.ico">>) -> static_file;
+route(<<"static/spa/spa.htm">>) -> login;
+route(<<"static/spa/index.htm">>) -> index;
 route(_) -> index.
--- samples/otp.mk
+++ samples/otp.mk
@@ -1,11 +1,17 @@
-empty :=
-ROOTS := apps deps
-space := $(empty) $(empty)
-comma := $(empty),$(empty)
-VSN   := $(shell git rev-parse HEAD | cut -c 1-6)
-DATE  := $(shell git show -s --format="%ci" HEAD | sed -e 's/\+/Z/g' -e 's/-/./g' -e 's/ /-/g' -e 's/:/./g')
+VM       := rels/web/files/vm.args
+SYS      := rels/web/files/sys.config
+PLT_NAME := ~/.n2o_dialyzer.plt
+ERL_ARGS := -args_file $(VM) -config $(SYS)
+RUN_DIR  ?= rels/web/devbox
+LOG_DIR  ?= rels/web/devbox/logs
+empty    :=
+ROOTS    := apps deps
+space    := $(empty) $(empty)
+comma    := $(empty),$(empty)
+VSN      := $(shell git rev-parse HEAD | cut -c 1-6)
+DATE     := $(shell git show -s --format="%ci" HEAD | sed -e 's/\+/Z/g' -e 's/-/./g' -e 's/ /-/g' -e 's/:/./g')
 ERL_LIBS := $(subst $(space),:,$(ROOTS))
-relx  := "{release,{$(RELEASE),\"$(VER)\"},[$(subst $(space),$(comma),$(APPS))]}.\\n{include_erts,true}.\
+relx     := "{release,{$(RELEASE),\"$(VER)\"},[$(subst $(space),$(comma),$(APPS))]}.\\n{include_erts,true}.\
 \\n{extended_start_script,true}.\\n{generate_start_script,true}.\\n{sys_config,\"$(SYS)\"}.\
 \\n{vm_args,\"$(VM)\"}.\\n{overlay,[{mkdir,\"log/sasl\"}]}."
 
@@ -14,12 +20,12 @@ compile: get-deps static-link
 delete-deps get-deps compile clean update-deps:
 	rebar $@
 .applist:
+	$(eval APPS := $(subst deps/,,$(subst apps/,,$(shell find apps deps -maxdepth 1 -mindepth 1 -type d))))
 	./depman.erl $(APPS) > $@
 $(RUN_DIR) $(LOG_DIR):
 	mkdir -p $(RUN_DIR) & mkdir -p $(LOG_DIR)
 console: .applist
-	ERL_LIBS=$(ERL_LIBS) erl $(ERL_ARGS) -eval \
-		'[ok = application:ensure_started(A, permanent) || A <- $(shell cat .applist)]'
+	ERL_LIBS=$(ERL_LIBS) erl $(ERL_ARGS) -eval '[application:start(A) || A <- $(shell cat .applist)]'
 start: $(RUN_DIR) $(LOG_DIR) .applist
 	RUN_ERL_LOG_GENERATIONS=1000 RUN_ERL_LOG_MAXSIZE=20000000 \
 	ERL_LIBS=$(ERL_LIBS) run_erl -daemon $(RUN_DIR)/ $(LOG_DIR)/ "exec $(MAKE) console"
@@ -28,7 +34,7 @@ attach:
 release:
 	echo $(shell echo $(relx) > relx.config) & relx
 stop:
-	kill -9 `ps ax -o pid= -o command=|grep $(RELEASE)|grep $(COOKIE)|awk '{print $$1}'`
+	@kill -9 $(shell ps ax -o pid= -o command=|grep $(RELEASE)|grep $(COOKIE)|awk '{print $$1}')
 $(PLT_NAME):
 	ERL_LIBS=deps dialyzer --build_plt --output_plt $(PLT_NAME) --apps $(APPS) || true
 dialyze: $(PLT_NAME) compile
