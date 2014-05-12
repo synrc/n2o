1ccc370faf1f7b38122a460f6dc846da1127317b
Maxim Sokhatsky <maxim@synrc.com>
2014-05-13 01:02:15 +0300

    omit log

--- samples/apps/n2o_sample/src/web_app.erl
+++ samples/apps/n2o_sample/src/web_app.erl
@@ -2,11 +2,12 @@
 -behaviour(application).
 -export([start/0, start/2, stop/1]).
 
-% this is for Erlang on Xen only
-
 start() -> start(normal, []).
 start(_StartType, _StartArgs) -> 
-    [ begin error_logger:info_msg("Application: ~p Start: ~p",[X,application:start(X)]) end || X <-
+
+    % this is for Erlang on Xen only
+
+    [ application:start(X) || X <-
         [crypto,sasl,ranch,cowlib,cowboy,gproc,syntax_tools,compiler,erlydtl,rest,xmerl,n2o]],
 
     web_sup:start_link().
