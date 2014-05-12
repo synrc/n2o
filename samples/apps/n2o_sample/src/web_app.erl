eb09f2f69d6197b11c96e93bc15c369335f64a29
Maxim Sokhatsky <maxim@synrc.com>
2014-05-13 00:59:46 +0300

    xmerl in app start

--- samples/apps/n2o_sample/src/web_app.erl
+++ samples/apps/n2o_sample/src/web_app.erl
@@ -1,12 +1,13 @@
 -module(web_app).
 -behaviour(application).
--export([start/2, stop/1]).
+-export([start/0, start/2, stop/1]).
 
 % this is for Erlang on Xen only
 
+start() -> start(normal, []).
 start(_StartType, _StartArgs) -> 
-    [ application:start(X) || X <-
-        [crypto,sasl,ranch,cowlib,cowboy,gproc,syntax_tools,compiler,erlydtl,rest,n2o]],
+    [ begin error_logger:info_msg("Application: ~p Start: ~p",[X,application:start(X)]) end || X <-
+        [crypto,sasl,ranch,cowlib,cowboy,gproc,syntax_tools,compiler,erlydtl,rest,xmerl,n2o]],
 
     web_sup:start_link().
 
