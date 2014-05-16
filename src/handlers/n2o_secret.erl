f47649f6e2e929a74c507ba38a4018d6f012c4ee
Maxim Sokhatsky <maxim@synrc.com>
2014-05-16 23:58:29 +0300

    rimemd160 -> sha256 with hmac

--- src/handlers/n2o_secret.erl
+++ src/handlers/n2o_secret.erl
@@ -4,11 +4,12 @@
 -export(?PICKLES_API).
 
 pickle(Data) ->
+    wf:info(?MODULE,"Secure: ~p",[Data]),
     Message = term_to_binary({Data,now()}),
     Padding = size(Message) rem 16,
     Bits = (16-Padding)*8, Key = secret(), IV = crypto:rand_bytes(16),
     Cipher = crypto:block_encrypt(aes_cbc128,Key,IV,<<Message/binary,0:Bits>>),
-    Signature = crypto:hash(ripemd160,<<Cipher/binary,IV/binary,Key/binary>>),
+    Signature = crypto:hmac(sha256,Key,<<Cipher/binary,IV/binary,Key/binary>>),
     base64:encode(<<IV/binary,Signature/binary,Cipher/binary>>).
 
 secret() -> wf:config(n2o,secret,<<"ThisIsClassified">>).
@@ -17,7 +18,7 @@ depickle(PickledData) ->
     try Key = secret(),
         Decoded = base64:decode(wf:to_binary(PickledData)),
         <<IV:16/binary,Signature:20/binary,Cipher/binary>> = Decoded,
-        Signature = crypto:hash(ripemd160,<<Cipher/binary,IV/binary,Key/binary>>),
+        Signature = crypto:hmac(sha256,Key,<<Cipher/binary,IV/binary,Key/binary>>),
         {Data,_Time} = binary_to_term(crypto:block_decrypt(aes_cbc128,Key,IV,Cipher)),
         Data
     catch _:_ -> undefined end.
