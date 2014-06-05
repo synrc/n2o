5923a40ce46b6b07c4fbfec7216eb8e0516d3532
Andy <andy.y2k@gmail.com>
2014-06-05 15:24:54 +0400

    Update n2o_bullet.erl

--- src/endpoints/cowboy/n2o_bullet.erl
+++ src/endpoints/cowboy/n2o_bullet.erl
@@ -49,12 +49,25 @@ info({bert,Message}, Req, State) ->
     wf:info(?MODULE,"Client BERT Binary Message: ~p Result: ~p",[Message,Term]),
     {reply,{binary,term_to_binary(Term)},Req,State};
 
+-record(binary, {
+    id :: integer(),
+    type :: integer(),
+    app :: integer(),
+    version :: integer(),
+    meta :: binary(),
+    data :: binary() }).
+
 info({binary,Message}, Req, State) ->
     wf_context:clear_actions(),
     Module = State#context.module,
     Term = try Module:event({binary,Message}) catch E:R -> wf:info(?MODULE,"Catch: ~p:~p~n~p", [E,R,n2o_error:stack()]), <<>> end,
     wf:info(?MODULE,"Client Raw Binary Message: ~p Result: ~p",[Message,Term]),
-    Res = case Term of _ when is_binary(Term) -> Term; _ -> term_to_binary(Term) end,
+    Res = case Term of
+        #binary{ id=Id, type=Type, app=App, version=Version, meta=Meta, data=Data } ->
+            <<"B"/binary, Id:32/little, Type:8, App:8, Version:8, bit_size(Meta):32/little, Meta/binary, Data/binary>>;
+        _ when is_binary(Term) -> Term;
+        _ -> term_to_binary(Term)
+        end,
     {reply,{binary,Res},Req,State};
 
 info({server,Message}, Req, State) ->
