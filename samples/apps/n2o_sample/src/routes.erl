b352864c18d6706859dc667261a542907d6c73a2
m-2k <andy.y2k@gmail.com>
2014-07-12 17:02:15 +0400

    Add dynamic routes support

--- samples/apps/n2o_sample/src/routes.erl
+++ samples/apps/n2o_sample/src/routes.erl
@@ -3,6 +3,12 @@
 -include_lib("n2o/include/wf.hrl").
 -export([init/2, finish/2]).
 
+%% U can use default dynamic routes or define custom static as this
+%% Just put needed module name to sys.config:
+%% {n2o, [{route,routes}]}
+%% Also with dynamic routes u must load all modules before starting Cowboy
+%% [code:ensure_loaded(M) || M <- [index, login, ... ]]
+
 finish(State, Ctx) -> {ok, State, Ctx}.
 init(State, Ctx) -> 
     Path = wf:path(Ctx#context.req),
@@ -14,7 +20,6 @@ route_prefix(P) -> route(P).
 
 route(<<>>)              -> index;
 route(<<"index">>)       -> index;
-% route(<<"index2">>)       -> index2;
 route(<<"login">>)       -> login;
 route(<<"favicon.ico">>) -> static_file;
 route(<<"static/spa/spa.htm">>) -> login;
--- samples/apps/n2o_sample/src/web_app.erl
+++ samples/apps/n2o_sample/src/web_app.erl
@@ -7,8 +7,13 @@ start(_StartType, _StartArgs) ->
 
     % this is for Erlang on Xen only
 
-    [ application:start(X) || X <-
+    [application:start(X) || X <-
         [crypto,sasl,ranch,cowlib,cowboy,gproc,syntax_tools,compiler,erlydtl,rest,xmerl,n2o]],
+        
+    % this is for n2o_route only
+
+    [code:ensure_loaded(M) || M <-
+        [index, login]],
 
     web_sup:start_link().
 
