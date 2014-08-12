3356bf6cc1abb6fe426b7983290392eac29d070e
Maxim Sokhatsky <maxim@synrc.com>
2014-08-13 01:25:15 +0300

    current mad

--- samples/apps/n2o_sample/src/web_app.erl
+++ samples/apps/n2o_sample/src/web_app.erl
@@ -9,13 +9,13 @@ start(_StartType, _StartArgs) ->
 
     % this is for Erlang on Xen only
 
-    [application:start(X) || X <-
-        [crypto,sasl,ranch,cowlib,cowboy,gproc,syntax_tools,compiler,erlydtl,rest,xmerl,n2o]],
+%    [application:start(X) || X <-
+%        [crypto,sasl,ranch,cowlib,cowboy,gproc,syntax_tools,compiler,erlydtl,rest,xmerl,n2o]],
         
     % this is for n2o_route only
 
-    [code:ensure_loaded(M) || M <-
-        [index, login]],
+%    [code:ensure_loaded(M) || M <-
+%        [index, login]],
 
     web_sup:start_link().
 
