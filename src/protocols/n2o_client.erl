17d534889e51a17b04c7536a10a4e1cbf739c966
Maxim Sokhatsky <maxim@synrc.com>
2014-08-29 13:20:56 +0300

    make nitrogen stack detachable

--- src/protocols/n2o_client.erl
+++ src/protocols/n2o_client.erl
@@ -10,7 +10,7 @@ info({client,Message}, Req, State) ->
 %    wf:info(?MODULE,"Client Message: ~p",[Message]),
     Module = State#context.module,
     try Module:event({client,Message}) catch E:R -> wf:info(?MODULE,"Catch: ~p:~p~n~p", wf:stack(E, R)) end,
-    {reply,wf:json([{eval,iolist_to_binary(n2o_nitrogen:render_actions(get(actions)))},
+    {reply,wf:json([%{eval,iolist_to_binary(n2o_nitrogen:render_actions(get(actions)))},
                     {data,binary_to_list(term_to_binary(Message))}]),Req,State};
 
 info({server,Message}, Req, State) ->
@@ -18,7 +18,7 @@ info({server,Message}, Req, State) ->
 %    wf:info(?MODULE,"Server Message: ~p",[Message]),
     Module = State#context.module,
     try Module:event({server,Message}),[] catch E:R -> wf:info(?MODULE,"Catch: ~p:~p~n~p", wf:stack(E, R)) end,
-    {reply,wf:json([{eval,iolist_to_binary(n2o_nitrogen:render_actions(get(actions)))},
+    {reply,wf:json([%{eval,iolist_to_binary(n2o_nitrogen:render_actions(get(actions)))},
                     {data,binary_to_list(term_to_binary(Message))}]),Req,State};
 
 info(Message, Req, State) -> {unknown,Message, Req, State}.
