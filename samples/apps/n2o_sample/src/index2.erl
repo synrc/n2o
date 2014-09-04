6b41abfe3dc96a05e7878d6ad0f83625feaa7fba
Maxim Sokhatsky <maxim@synrc.com>
2014-09-04 11:50:45 +0300

    default is pickle Nitrogen protol

--- samples/apps/n2o_sample/src/index2.erl
+++ samples/apps/n2o_sample/src/index2.erl
@@ -3,7 +3,6 @@
 -include_lib("n2o/include/wf.hrl").
 
 peer()    -> io_lib:format("~p",[wf:peer(?REQ)]).
-message(Message) -> wf:js_escape(wf:html_encode(Message)).
 main()    -> #dtl{file="index",app=n2o_sample,bindings=[{body,body()}]}.
 body() ->
     {ok,Pid} = wf:comet(fun() -> chat_loop() end), 
@@ -11,7 +10,7 @@ body() ->
       #button{id=send,body="Chat",postback={chat,Pid},source=[message]} ].
 
 event(init) -> wf:reg(room).
-event(#ev{payload={chat,Pid}},#context{req=Req}=Cx) -> Pid ! {wf:peer(Req), wf:qp(message,Cx)};
+event(#ev{payload={chat,Pid}},#context{req=Req}=Cx) -> Pid ! {peer(), wf:qp(message,Cx)};
 event(Event,_) -> skip.
 
 chat_loop() ->
