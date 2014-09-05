b2dfb8b0eb66af9d4e4dca490197f33826044ac4
Maxim Sokhatsky <maxim@synrc.com>
2014-09-05 11:05:52 +0300

    NewState in n2o_rails

--- src/rails/n2o_rails.erl
+++ src/rails/n2o_rails.erl
@@ -12,7 +12,7 @@ info({rails,_,_,_}=Event, Req, State) ->
     {Result,NewState} = 
          try events(Event,State) 
          catch E:R -> wf:info(?MODULE,"Catch: ~p:~p~n~p", wf:stack(E, R)), {<<>>,State} end,
-    {reply,Result,wf_core:set_cookies(wf:cookies(),Req),State};
+    {reply,Result,wf_core:set_cookies(wf:cookies(),Req),NewState};
 
 info({flush,Actions}, Req, State) ->
     wf_context:clear_actions(),
