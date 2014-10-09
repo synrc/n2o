35307babf0ecf565f71c8ae580a3c3c876f8da6d
Maxim Sokhatsky <maxim@synrc.com>
2014-10-09 13:17:21 +0300

    autoinit

--- priv/n2o.js
+++ priv/n2o.js
@@ -2,6 +2,7 @@
 // N2O CORE
 
 var active      = false,
+    autoinit    = true,
     protocol    = window.location.protocol == 'https:' ? "wss://" : "ws://",
     querystring = window.location.pathname + window.location.search,
     host        = null == transition.host ? window.location.hostname : transition.host,
@@ -16,4 +17,4 @@ function N2O_start() {
 }
 
 //WebSocket = undefined; // test XHR fallback
-N2O_start();
+if (autoinit) N2O_start();
