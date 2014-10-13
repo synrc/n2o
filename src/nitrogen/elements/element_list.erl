f940425abc13d5e31aaaaf28dc7a092a427a660e
Maxim Sokhatsky <maxim@synrc.com>
2014-10-13 12:55:57 +0300

    ol

--- src/nitrogen/elements/element_list.erl
+++ src/nitrogen/elements/element_list.erl
@@ -3,9 +3,7 @@
 -include_lib("n2o/include/wf.hrl").
 -compile(export_all).
 
-reflect() -> record_info(fields, list).
-
-render_element(Record) -> 
+render_element(Record = #list{}) -> 
   Tag = case Record#list.numbered of true -> <<"ol">>; _ -> <<"ul">> end,
 
   wf_tags:emit_tag(Tag, wf:render(Record#list.body), [
@@ -13,3 +11,4 @@ render_element(Record) ->
     {<<"class">>, Record#list.class},
     {<<"style">>, Record#list.style} | Record#list.data_fields
   ]).
+
