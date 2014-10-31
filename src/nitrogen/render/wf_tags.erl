15aa5b8549855eee0c498ef8eba2eaad1ccc7863
Maxim Sokhatsky <maxim@synrc.com>
2014-10-31 22:59:27 +0200

    fix XHTML tags

--- src/nitrogen/render/wf_tags.erl
+++ src/nitrogen/render/wf_tags.erl
@@ -10,7 +10,7 @@
               orelse Tag == <<"col">>    orelse Tag == <<"command">> orelse Tag == <<"option">>
               orelse Tag == <<"keygen">> orelse Tag == <<"source">>)).
 
-emit_tag(TagName, Props) -> [<<"<">>,TagName] ++ write_props(Props) ++ [<<">">>].
+emit_tag(TagName, Props) -> [<<"<">>,TagName] ++ write_props(Props) ++ [<<"/>">>].
 emit_tag(TagName, undefined, Props) -> emit_tag(TagName, [], Props);
 emit_tag(TagName, [undefined], Props) -> emit_tag(TagName, [], Props);
 emit_tag(TagName, [], Props) when ?VOID(TagName) -> emit_tag(TagName, Props);
