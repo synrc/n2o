34e1d9a92a727320dbd68c7c2f9e0081351ac4c2
G-Grand <ggrand.bs@gmail.com>
2014-12-03 15:33:52 +0200

    update geting Date from input date field
    Now the date is converted into a normal form or converted to an empty string

--- priv/protocols/nitrogen.js
+++ priv/protocols/nitrogen.js
@@ -7,8 +7,7 @@ function querySourceRaw(Id) {
         case 'fieldset': val = document.querySelector('#' + Id + ' :checked');
                          val = val ? val.value : ""; break;
         case 'radio': case 'checkbox': val = el.checked ? el.value : ""; break;
-        case  'date': val = /^(\d{4})-(\d{2})-(\d{2})$/.exec(el.value);
-                      val = (val != null) ? "{"+ val[1] +","+ val[2] +","+ val[3] +"}" : ""; break;
+        case  'date': val = getErlDate(el); break;
         default:         var edit = el.getAttribute('contenteditable');
                          if (edit && edit === 'true') val = el.innerHTML;
                                                  else val = el.value; }
@@ -19,3 +18,14 @@ function querySource(Id) {
     var qs = querySourceRaw(Id);
     if ("" == qs) return bin('');
              else return utf8_toByteArray(qs); }
+
+function getErlDate(el) {
+    var val = /^(\d{4})-(\d{2})-(\d{2})$/.exec(el.value);
+    if(val != null){
+        var uDate = new Date(parseInt(val[1]),parseInt(val[2])-1,parseInt(val[3]));
+        val[2] = (uDate.getMonth() < 9) ? "0" + (uDate.getMonth()+1) : (uDate.getMonth()+1);
+        val[3] = (uDate.getDate()  < 10) ? "0" + uDate.getDate() : uDate.getDate();
+        val = "{"+ uDate.getFullYear() +","+ val[2] +","+ val[3] +"}";
+    } else val = "";
+    return val;
+}
