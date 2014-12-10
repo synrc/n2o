2951a7fa352669349cb93f93900a4582f3ca1b32
G-Grand <ggrand.bs@gmail.com>
2014-12-10 16:52:06 +0200

    update date validation

--- priv/protocols/nitrogen.js
+++ priv/protocols/nitrogen.js
@@ -23,9 +23,11 @@ function getErlDate(el) {
     var val = /^(\d{4})-(\d{1,2})-(\d{1,2})$/.exec(el.value);
     if(val != null){
         var uDate = new Date(parseInt(val[1]),parseInt(val[2])-1,parseInt(val[3]));
-        val[2] = (uDate.getMonth() < 9) ? "0" + (uDate.getMonth()+1) : (uDate.getMonth()+1);
-        val[3] = (uDate.getDate()  < 10) ? "0" + uDate.getDate() : uDate.getDate();
-        val = "{"+ uDate.getFullYear() +","+ val[2] +","+ val[3] +"}";
+        if( (uDate.getMonth()+1)==parseInt(val[2]) && (uDate.getDate())==parseInt(val[3]) && (uDate.getFullYear())==parseInt(val[1]) ) {
+            val[2] = (uDate.getMonth() < 9) ? "0" + (uDate.getMonth()+1) : (uDate.getMonth()+1);
+            val[3] = (uDate.getDate()  < 10) ? "0" + uDate.getDate() : uDate.getDate();
+            val = "{"+ uDate.getFullYear() +","+ val[2] +","+ val[3] +"}";
+        }else val = "";
     } else val = "";
     return val;
 }
