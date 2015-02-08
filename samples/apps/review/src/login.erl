554f3b4343ec4de64863a133dd40645138d33889
Namdak Tonpa <maxim@synrc.com>
2015-02-09 01:51:13 +0200

    fix login

--- samples/apps/review/src/login.erl
+++ samples/apps/review/src/login.erl
@@ -7,10 +7,10 @@ main() -> #dtl{file="login",app=review,bindings=[{body,body()},{folders,folders(
 folders() -> string:join([filename:basename(F)||F<-filelib:wildcard(code:priv_dir(review)++"/snippets/*/")],",").
 
 body() ->
- [ #form{body=[#span{id=display}, #br{},
-            #span{body="Login: "}, #textbox{id=user,autofocus=true}, #br{},
-            #span{body="Join/Create Feed: "}, #textbox{style="width:160px;",id=pass},
-            #button{body="Login",postback=login,source=[user,pass],type="submit"}]}  ].
+ [ #span   { id=display },                #br{},
+   #span   { body="Login: " },            #textbox{id=user,autofocus=true}, #br{},
+   #span   { body="Join/Create Feed: " }, #textbox{id=pass},
+   #button { body="Login",postback=login,source=[user,pass]}].
 
 event(init) -> 
     [ index:event({client,{"feed",element(2,F#feed.id)}}) || F <-kvs:all(feed)],
