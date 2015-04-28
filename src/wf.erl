b390733f8571d28007da9cd0535e9ba6c660a756
Namdak Tonpa <maxim@synrc.com>
2015-04-28 12:26:02 +0300

    2.4

--- src/wf.erl
+++ src/wf.erl
@@ -259,7 +259,7 @@ config(Key) -> config(n2o, Key, "").
 config(App, Key) -> config(App,Key, "").
 config(App, Key, Default) -> wf_utils:config(App, Key, Default).
 
-version() -> "2.5".
+version() -> "2.4".
 
 setkey(Name,Pos,List,New) ->
     case lists:keyfind(Name,Pos,List) of
