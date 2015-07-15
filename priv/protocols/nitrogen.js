6988476b3e178e0c29f1c85eb4ab755fea060af4
MIkhail Gulyaev <gmichaelat@gmail.com>
2015-07-15 16:03:16 +0500

    Fix error in querySourceRaw
    Fix error in querySourceRaw with getAttribute('type')

--- priv/protocols/nitrogen.js
+++ priv/protocols/nitrogen.js
@@ -3,12 +3,12 @@
 function querySourceRaw(Id) {
     var val, el = document.getElementById(Id);
     if (!el) return "";
-    switch (el.getAttribute("type")) {
+    switch (el.type) {
         case 'fieldset': val = document.querySelector('#' + Id + ' :checked');
                          val = val ? val.value : ""; break;
         case 'radio': case 'checkbox': val = el.checked ? el.value : ""; break;
         case  'date': val = getErlDate(el); break;
-        default:         var edit = el.getAttribute('contenteditable');
+        default:         var edit = el.contentEditable;
                          if (edit && edit === 'true') val = el.innerHTML;
                                                  else val = el.value; }
     return val; }
