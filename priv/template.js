f01c6cc6f860b341cdc093ba2d6e12c06708d817
BigData89 <brichkovskiy777@mail.ru>
2015-07-16 14:32:19 +0300

    Added compatibility with browsers that do not support full functionality of DOMParser

--- priv/template.js
+++ priv/template.js
@@ -15,6 +15,15 @@ function template(html, data) { // template("{this.name}",{name:"Maxim"})
     return new Function(code.replace(/[\r\t\n]/g, '')).apply(data); }
 
 function xml(html) { return new DOMParser().parseFromString(html, "application/xhtml+xml").firstChild; }
+
 function dom(html) {
-    var dom =  new DOMParser().parseFromString(html, "text/html")
-              .firstChild.getElementsByTagName("body")[0].firstChild; return dom; }
+try {
+  var dom =  new DOMParser().parseFromString(html, "text/html")
+                .firstChild.getElementsByTagName("body")[0].firstChild;
+  return dom; } catch (ex) {
+      var tempContainer = document.createElement("DIV");
+      tempContainer.innerHTML = html;
+      var dom = tempContainer.firstChild;
+      return dom; }
+}
+
