3e801c610af9f2d50adb3d5c8c9c88f95d98c485
Namdak Tonpa <maxim@synrc.com>
2015-09-21 20:28:20 +0300

    use bind

--- samples/apps/review/src/upload.erl
+++ samples/apps/review/src/upload.erl
@@ -6,10 +6,10 @@
 
 render_element(#upload{id=Id} = U) ->
     Uid = case Id of undefined -> wf:temp_id(); I -> I end,
-    wf:wire(select()),
-    wf:wire(browse()),
-    wf:wire(start()),
-    wf:wire(stop()),
+    bind(ftp_open,  click,  "qi('upload').click(); e.preventDefault();"),
+    bind(ftp_start, click,  "ftp.start();"),
+    bind(ftp_stop,  click,  "ftp.stop();"),
+    bind(nitro:to_atom(Uid), change, "ftp.init(this.files[0],1);"),
     Upload = #panel  { body = [
              #input  { id   = Uid,         type    = <<"file">>, style = "display:none" },
              #span   { id   = ftp_status,  body    = [] },
@@ -19,7 +19,5 @@ render_element(#upload{id=Id} = U) ->
              #button { id   = ftp_stop,    body = "Stop" }
     ] } ] }, wf:render(Upload).
 
-browse() -> "{ var x=qi('ftp_open');  if (x) x.addEventListener('click', function(e) { qi('upload').click(); e.preventDefault(); }); }".
-start()  -> "{ var x=qi('ftp_start'); if (x) x.addEventListener('click', function(e) { ftp.start(); }); }".
-stop()   -> "{ var x=qi('ftp_stop');  if (x) x.addEventListener('click', function(e) { ftp.stop(); }); }".
-select() -> "{ var x=qi('upload');    if (x) x.addEventListener('change',function(e) { ftp.init(this.files[0],1); }); }".
+bind(Control,Event,Code) ->
+    wf:wire(#bind{target=Control,type=Event,postback=Code}).
