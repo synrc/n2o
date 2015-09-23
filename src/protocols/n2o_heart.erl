5cb7806f66e8b9e6beab2433d7a8bf7fe59680e5
Namdak Tonpa <maxim@synrc.com>
2015-09-23 23:42:26 +0300

    n2o remote protocol

--- src/protocols/n2o_heart.erl
+++ src/protocols/n2o_heart.erl
@@ -11,4 +11,30 @@ info({text,<<"N2O,",Process/binary>> = _InitMarker}=Message, Req, State) ->
     wf:info(?MODULE,"N2O INIT: ~p",[Message]),
     n2o_proto:push({init,Process},Req,State,n2o_proto:protocols(),[]);
 
+% ETS access protocol
+info({cache,Operation,Key,Value}=Message,Req,State)   -> {reply, <<"OK">>, Req, State};
+info({session,Operation,Key,Value}=Message,Req,State) -> {reply, <<"OK">>, Req, State};
+
+% MQ protocol
+info({pub,Topic,Message}=Message,Req,State) -> {reply, <<"OK">>, Req, State};
+info({sub,Topic,Args}=Message,Req,State)    -> {reply, <<"OK">>, Req, State};
+info({unsub,Topic}=Message,Req,State)       -> {reply, <<"OK">>, Req, State};
+
+% WF protocol
+info({q,Operation,Key}=Message,Req,State)                -> {reply, <<"OK">>, Req, State};
+info({qc,Operation,Key}=Message,Req,State)               -> {reply, <<"OK">>, Req, State};
+info({cookie,Operation,Key,Value}=Message,Req,State)     -> {reply, <<"OK">>, Req, State};
+info({wire,Parameter}=Message,Req,State)                 -> {reply, <<"OK">>, Req, State};
+info({update,Target,Elements}=Message,Req,State)         -> {reply, <<"OK">>, Req, State};
+info({insert_top,Target,Elements}=Message,Req,State)     -> {reply, <<"OK">>, Req, State};
+info({insert_bottom,Target,Elements}=Message,Req,State)  -> {reply, <<"OK">>, Req, State};
+
+% ASYNC protocol
+info({start,Handler}=Message,Req,State)          -> {reply, <<"OK">>, Req, State};
+info({stop,Name}=Message,Req,State)              -> {reply, <<"OK">>, Req, State};
+info({restart,Name}=Message,Req,State)           -> {reply, <<"OK">>, Req, State};
+info({async,Name,Function}=Message,Req,State)    -> {reply, <<"OK">>, Req, State};
+info({flush}=Message,Req,State)                  -> {reply, <<"OK">>, Req, State};
+info({flush,Key}=Message,Req,State)              -> {reply, <<"OK">>, Req, State};
+
 info(Message, Req, State) -> {unknown,Message, Req, State}.
