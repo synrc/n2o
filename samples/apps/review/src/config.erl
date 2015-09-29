9758e0b0d23f8ba5a594d58a48cfd4e14aa5bae3
Namdak Tonpa <maxim@synrc.com>
2015-09-29 17:41:49 +0300

    samples fix

--- samples/apps/review/src/config.erl
+++ samples/apps/review/src/config.erl
@@ -6,12 +6,13 @@ log_modules() -> % any
   [
     login,
 %    wf_convert,
-%    n2o_file,
+    n2o_file,
     n2o_async,
     n2o_proto,
     n2o_client,
     n2o_stream,
     n2o_nitrogen,
+    n2o_session,
     kvs,
     store_mnesia,
     index
