0be0268ebc73dae2c5cc4eda56e41f14b37e806c
Namdak Tonpa <maxim@synrc.com>
2015-10-01 04:55:42 +0300

    code

--- samples/apps/review/src/index.erl
+++ samples/apps/review/src/index.erl
@@ -6,12 +6,13 @@
 
 main() ->
     case wf:user() of
-         undefined -> wf:redirect("login.htm"), #dtl{};
+         undefined -> wf:redirect("login.htm"), redirect_wait();
          _ -> #dtl{file = "index", app=review,bindings=[{body,body()},{list,list()}]} end.
 
+redirect_wait() -> #dtl{}.
+list() -> "<iframe src=http://synrc.com/apps/"++code()++" frameborder=0 width=700 height=1250></iframe>".
 code() -> case wf:q(<<"code">>) of undefined  -> "../privacy.htm";
                                     Code -> wf:to_list(wf:depickle(Code)) end.
-list() -> "<iframe src=http://synrc.com/apps/"++code()++" frameborder=0 width=700 height=1250></iframe>".
 
 body() ->
     wf:update(heading,#b{id=heading,body="Review: " ++ code()}),
@@ -29,6 +30,25 @@ event(init) ->
     [ event({client,{E#entry.from,E#entry.media}}) || E <-
        lists:reverse(kvs:entries(kvs:get(feed,{room,Room}),entry,10)) ];
 
+event(logout) ->
+    wf:logout(),
+    wf:redirect("login.htm");
+
+event(chat) ->
+    wf:info(?MODULE,"Chat pressed~n",[]),
+    User = wf:user(),
+    Message = wf:q(message),
+    Room = code(),
+    kvs:add(#entry{id=kvs:next_id("entry",1),from=wf:user(),feed_id={room,Room},media=Message}),
+    wf:send({topic,Room},#client{data={User,Message}});
+
+event(#client{data={User,Message}}) ->
+    wf:wire(#jq{target=message,method=[focus,select]}),
+    HTML = wf:html_encode(wf:to_list(Message)),
+    wf:info(?MODULE,"HTML: ~tp~n",[HTML]),
+    DTL = #dtl{file="message",app=review,bindings=[{user,User},{color,"gray"},{message,HTML}]},
+    wf:insert_top(history, wf:jse(wf:render(DTL)));
+
 event(#bin{data=Data}) ->
     wf:info(?MODULE,"Binary Delivered ~p~n",[Data]),
     #bin{data = "SERVER"};
@@ -37,27 +57,9 @@ event(#ftp{}=Data) ->
     wf:info(?MODULE,"FTP Delivered ~p~n",[Data]),
     ok;
 
-event(chat) ->
-    wf:info(?MODULE,"Chat pressed~n",[]),
-    User = wf:user(),
-    Message = wf:q(message),
-    Room = code(),
-    kvs:add(#entry{id=kvs:next_id("entry",1),from=wf:user(),feed_id={room,Room},media=Message}),
-    wf:send({topic,Room},{client,{User,Message}});
-
-event({client,{User,Message}}=M) ->
-    wf:wire(#jq{target=message,method=[focus,select]}),
-    HTML = wf:html_encode(wf:to_list(Message)),
-    wf:info(?MODULE,"HTML: ~tp~n",[HTML]),
-    DTL = #dtl{file="message",app=review,bindings=[{user,User},{color,"gray"},{message,HTML}]},
-    wf:insert_top(history, wf:jse(wf:render(DTL)));
-
-event(#client{data=Data}) ->
-    wf:info(?MODULE,"Client Delivered ~p~n",[Data]),
-    ok;
-
-event(logout) -> wf:logout(), wf:redirect("login.htm");
-event(Event) -> wf:info(?MODULE,"Event: ~p", [Event]).
+event(Event) ->
+    wf:info(?MODULE,"Event: ~p", [Event]),
+    ok.
 
 loop(M) ->
     DTL = #dtl{file="message",app=review,bindings=[{user,"system"},{message,M},{color,"silver"}]},
