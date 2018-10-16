344a535a8cfaf59b0d67debddb6ab3c9419d6043
Namdak Tonpa <maxim@synrc.com>
2018-10-16 20:47:15 +0300

    switch nitro to master in sample

--- samples/apps/review/src/config.erl
+++ samples/apps/review/src/config.erl
@@ -5,12 +5,12 @@ log_level() -> info.
 log_modules() -> % any
   [
     login,
-%    wf_convert,
-%    n2o_file,
+    wf_convert,
+    n2o_file,
     n2o_async,
     n2o_proto,
-%    n2o_client,
-%    n2o_static,
+    n2o_client,
+    n2o_static,
     n2o_stream,
     n2o_nitrogen,
     n2o_session,
