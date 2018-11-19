1b28d556d8f1af6f7f2d6b46ea7b117b58a677bb
Namdak Tonpa <maxim@synrc.com>
2018-11-19 08:39:15 +0200

    fix io token

--- priv/n2o.js
+++ priv/n2o.js
@@ -29,14 +29,13 @@ var $io = {}; $io.on = function onio(r, cb) {
     if (is(r, 3, 'io')) {
         if (r.v[2].v != undefined && r.v[2].v[1] != undefined &&
             r.v[2].v.length == 2 && (r.v[2].v[0].v == "Token" || r.v[2].v[0].v == "Auth")) {
-            tok = String.fromCharCode.apply(null, new Uint8Array(r.v[2].v[1].v));
             console.log("WS Subscribe");
-            localStorage.setItem("token",tok);
+            localStorage.setItem("token",utf8_dec(r.v[2].v[1].v));
         }
         try { eval(utf8_dec(r.v[1].v));
               if (typeof cb == 'function') cb(r);
               return { status: "ok" };
-        } catch (e)  { console.log(r);
+        } catch (e)  { console.log("Eval error: "+r);
                        return { status: '' }; }
     } else return { status: '' };
 }
