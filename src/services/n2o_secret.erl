620b7c7a781050b2ef5f7a83d8bbec2bb955df56
Namdak Tonpa <maxim@synrc.com>
2018-11-22 10:11:21 +0200

    sync

--- src/services/n2o_secret.erl
+++ src/services/n2o_secret.erl
@@ -1,7 +1,7 @@
 -module(n2o_secret).
 -description('N2O HMAC AES/CBC-128').
 -include("n2o.hrl").
--export([pickle/1,depickle/1,secret/0]).
+-export([pickle/1,depickle/1,secret/0,hex/1,unhex/1]).
 
 pickle(Data) ->
     Message = term_to_binary(Data),
@@ -9,14 +9,18 @@ pickle(Data) ->
     Bits = (16-Padding)*8, Key = secret(), IV = crypto:strong_rand_bytes(16),
     Cipher = crypto:block_encrypt(aes_cbc128,Key,IV,<<Message/binary,0:Bits>>),
     Signature = crypto:hmac(sha256,Key,<<Cipher/binary,IV/binary>>),
-    nitro_conv:hex(<<IV/binary,Signature/binary,Cipher/binary>>).
-
-secret() -> application:get_env(n2o,secret,<<"ThisIsClassified">>).
+    hex(<<IV/binary,Signature/binary,Cipher/binary>>).
 
 depickle(PickledData) ->
     try Key = secret(),
-        Decoded = nitro_conv:unhex(iolist_to_binary(PickledData)),
+        Decoded = unhex(iolist_to_binary(PickledData)),
         <<IV:16/binary,Signature:32/binary,Cipher/binary>> = Decoded,
         Signature = crypto:hmac(sha256,Key,<<Cipher/binary,IV/binary>>),
         binary_to_term(crypto:block_decrypt(aes_cbc128,Key,IV,Cipher),[safe])
     catch _:_ -> <<>> end.
+
+secret() -> application:get_env(n2o,secret,<<"ThisIsClassified">>).
+hex(Bin) -> << << (digit(A1)),(digit(A2)) >> || <<A1:4,A2:4>> <= Bin >>.
+unhex(Hex) -> << << (erlang:list_to_integer([H1,H2], 16)) >> || <<H1,H2>> <= Hex >>.
+digit(X) when X >= 0 andalso X =< 9 -> X + 48;
+digit(X) when X >= 10 andalso X =< 15 -> X + 87.
