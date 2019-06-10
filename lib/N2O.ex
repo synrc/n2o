1f0228d02226a2d8162e09011a71186bcf1e658d
Namdak Tonpa <maxim@synrc.com>
2019-06-10 13:14:51 +0300

    pi.ex

--- lib/N2O.ex
+++ lib/N2O.ex
@@ -3,5 +3,6 @@ defmodule N2O do
   import  Record, only: [defrecord: 2, extract: 2]
 
   defrecord :cx, extract(:cx, from_lib: "n2o/include/n2o.hrl")
+  defrecord :pi, extract(:pi, from_lib: "n2o/include/n2o.hrl")
 
 end
