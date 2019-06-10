b2b9240e79ec8ab3c313efce85d05daa64e16593
Namdak Tonpa <maxim@synrc.com>
2019-06-10 11:05:13 +0300

    app.src

--- mix.exs
+++ mix.exs
@@ -1,19 +1,17 @@
 defmodule N2O.Mixfile do
   use Mix.Project
-  def project do
-    [app: :n2o,
-     version: "6.6.6",
-     description: "N2O DAS MQTT TCP WebSocket",
-     package: package(),
-     deps: deps()]
-  end
+  def deps, do: [ {:ex_doc, ">= 0.0.0", only: :dev}]
   def application, do: [mod: {:n2o, []}]
-  defp package do
-    [files: ~w(doc include man priv src test mix.exs rebar.config LICENSE),
-     licenses: ["ISC"],
-     links: %{"GitHub" => "https://github.com/synrc/n2o"}]
+  def project do
+    [ app: :n2o,
+      version: "6.6.6",
+      description: "N2O DAS MQTT TCP WebSocket",
+      package: package(),
+      deps: deps()]
   end
-  defp deps do
-     [{:ex_doc, ">= 0.0.0", only: :dev}]
+  def package do
+    [ files: ~w(doc include man priv src test mix.exs rebar.config LICENSE),
+      licenses: ["ISC"],
+      links: %{"GitHub" => "https://github.com/synrc/n2o"}]
   end
 end
