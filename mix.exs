1fbd169fc01826ec3e0a128beb11e9fc1b766cfe
Namdak Tonpa <maxim@synrc.com>
2019-07-06 17:01:32 +0300

    rebar3 dialyzer

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule N2O.Mixfile do
   def application, do: [mod: {:n2o, []}]
   def project do
     [ app: :n2o,
-      version: "6.7.2",
+      version: "6.7.3",
       description: "N2O MQTT TCP WebSocket",
       package: package(),
       deps: deps()]
