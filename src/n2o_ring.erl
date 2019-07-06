821f4acd741f3609b9fac0298c7033fbe830551c
Namdak Tonpa <maxim@synrc.com>
2019-07-06 04:09:44 +0300

    new api

--- src/n2o_ring.erl
+++ src/n2o_ring.erl
@@ -1,44 +1,54 @@
 -module(n2o_ring).
 -copyright('Copyright (c) 2016 Carlos Galdino').
 -license('MIT').
--description('N2O Ring').
 -export([add/2,contains/2,lookup/2,members/1,new/1,new/2,remove/2,size/1,send/2]).
--export_type([ring/0]).
 -define(HASH, sha256).
-
-send(ws,Msg) ->
-    R = application:get_env(n2o,ws_ring,n2o_ring:new(1,[1])),
-    Node = n2o_ring:lookup(Msg, R),
-    n2o_pi:send(ws,Node,Msg).
 
 -type num_vnodes() :: pos_integer().
 -type node_entry() :: term().
 -type key() :: term().
 -type position() :: binary().
-
 -type positions() :: [{position(), node_entry()}].
 -type nodes() :: [node_entry()].
-
 -type inner_ring() :: gb_trees:tree(position(), node_entry()).
+-type ring_index() :: {num_vnodes(), inner_ring()}.
+-type ring() :: tcp | ws | mqtt.
 
--opaque ring() :: {num_vnodes(), inner_ring()}.
+-spec add(node_entry(), ring()) -> ring_index().
+-spec contains(node_entry(), ring()) -> boolean().
+-spec lookup(key(), ring()) -> node_entry() | {error, empty_ring}.
+-spec position_node(num_vnodes(), node_entry()) -> positions().
+-spec build_ring(positions(), inner_ring()) -> inner_ring().
+-spec build_ring(positions()) -> inner_ring().
+-spec size(Ring :: ring()) -> non_neg_integer().
+-spec members(ring()) -> nodes().
+-spec new(nodes()) -> ring().
+-spec new(num_vnodes(), nodes()) -> ring_index().
+-spec remove(node_entry(), ring()) -> ring_index().
 
--spec add(node_entry(), Ring :: ring()) -> ring().
-add(Node, {NumVNodes, InnerRing}) ->
+tab2ring(ws)       -> ws_ring;
+tab2ring(mqtt)     -> mqtt_ring;
+tab2ring(tcp)      -> tcp_ring.
+ring(Tab)          -> application:get_env(n2o,tab2ring(Tab),n2o_ring:new(1,[1])).
+lookup(Tab,Term)   -> lookup_index(Term, ring(Tab)).
+contains(Tab,Term) -> contains_index(Term, ring(Tab)).
+add(Tab,Node)      -> add_index(Node, ring(Tab)).
+remove(Tab,Node)   -> remove_index(Node, ring(Tab)).
+size(Tab)          -> size_index(Tab).
+send(Tab,Msg)      -> n2o_pi:send(Tab,lookup_index(Msg, ring(Tab)),Msg).
+members(Tab)       -> members_index(ring(Tab)).
+
+add_index(Node, {NumVNodes, InnerRing}) ->
     NewInnerRing = build_ring(position_node(NumVNodes, Node), InnerRing),
     {NumVNodes, NewInnerRing}.
 
-%% @doc Returns true if the given node is present in the ring, otherwise false.
--spec contains(node_entry(), Ring :: ring()) -> boolean().
-contains(Node, {_NumVNodes, InnerRing}) ->
+contains_index(Node, {_NumVNodes, InnerRing}) ->
     case gb_trees:lookup(chash(Node), InnerRing) of
         none -> false;
         {value, _} -> true
     end.
 
-%% @doc Returns the node associated with the given key. Returns an error if the ring is empty.
--spec lookup(key(), Ring :: ring()) -> node_entry() | {error, empty_ring}.
-lookup(Key, {_NumVNodes, InnerRing}) ->
+lookup_index(Key, {_NumVNodes, InnerRing}) ->
     case gb_trees:is_empty(InnerRing) of
         true -> {error, empty_ring};
         false ->
@@ -50,55 +60,24 @@ lookup(Key, {_NumVNodes, InnerRing}) ->
             end
     end.
 
-%% @doc Returns the ordered list of nodes in the ring.
--spec members(Ring :: ring()) -> nodes().
-members({_NumVNodes, InnerRing}) ->
-    lists:usort(gb_trees:values(InnerRing)).
-
-%% @doc Creates a new ring without virtual nodes.
--spec new(nodes()) -> ring().
-new(Nodes) ->
-    new(1, Nodes).
-
-%% @doc Creates a new ring with `NumVNodes' of virtual nodes.
--spec new(num_vnodes(), nodes()) -> ring().
 new(NumVNodes, Nodes) ->
     Ring = build_ring(lists:flatten([position_node(NumVNodes, Node) || Node <- Nodes])),
     {NumVNodes, Ring}.
 
-%% @doc Removes the given node (and its virtual nodes) from the ring if the node is present in the ring, otherwise does nothing. Returns the new ring.
--spec remove(node_entry(), Ring :: ring()) -> ring().
-remove(Node, {NumVNodes, InnerRing}) ->
+remove_index(Node, {NumVNodes, InnerRing}) ->
     Positions = position_node(NumVNodes, Node),
     NewInnerRing = lists:foldl(fun({Pos, _}, Tree) -> gb_trees:delete_any(Pos, Tree) end, InnerRing, Positions),
     {NumVNodes, NewInnerRing}.
 
-%% @doc Returns the number of nodes (including virtual nodes) in the ring.
--spec size(Ring :: ring()) -> non_neg_integer().
-size({_NumVNodes, InnerRing}) ->
-    gb_trees:size(InnerRing).
-
--spec build_ring(positions()) -> inner_ring().
-build_ring(Nodes) ->
-    gb_trees:from_orddict(lists:keysort(1, Nodes)).
-
--spec build_ring(positions(), inner_ring()) -> inner_ring().
-build_ring(Nodes, Ring) ->
-    lists:foldl(fun({Pos, Node}, Tree) -> gb_trees:insert(Pos, Node, Tree) end, Ring, Nodes).
-
+size_index({_NumVNodes, InnerRing}) -> gb_trees:size(InnerRing).
+build_ring(Nodes) -> gb_trees:from_orddict(lists:keysort(1, Nodes)).
+build_ring(Nodes, Ring) -> lists:foldl(fun({Pos, Node}, Tree) -> gb_trees:insert(Pos, Node, Tree) end, Ring, Nodes).
+members_index({_NumVNodes, InnerRing}) -> lists:usort(gb_trees:values(InnerRing)).
+new(Nodes) -> new(1, Nodes).
 chash(X) -> crypto:hash(?HASH, term_to_binary(X)).
-
-chash(X, Y) ->
-    XBin = term_to_binary(X),
-    YBin = term_to_binary(Y),
-    crypto:hash(?HASH, <<XBin/binary, YBin/binary>>).
-
-position_node(Node) ->
-    {chash(Node), Node}.
-
--spec position_node(num_vnodes(), node_entry()) -> positions().
-position_node(1, Node) ->
-    [position_node(Node)];
+chash(X, Y) -> XBin = term_to_binary(X), YBin = term_to_binary(Y), crypto:hash(?HASH, <<XBin/binary, YBin/binary>>).
+position_node(Node) -> {chash(Node), Node}.
+position_node(1, Node) -> [position_node(Node)];
 position_node(NumVNodes, Node) ->
     Replicas = [{chash(Node, Idx), Node} || Idx <- lists:seq(1, NumVNodes - 1)],
     [position_node(Node) | Replicas].
