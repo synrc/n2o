550021d67110a9ed21a1ccd7ee43594569c44916
Namdak Tonpa <maxim@synrc.com>
2019-08-06 20:53:26 +0300

    n2o_xml

--- src/services/n2o_xml.erl
+++ src/services/n2o_xml.erl
@@ -1,18 +1,19 @@
 -module(n2o_xml).
 -description('N2O XML Formatter').
 -include("n2o.hrl").
+-include_lib("xmerl/include/xmerl.hrl").
+-compile(export_all).
 -export([encode/1,decode/1]).
 
-serialize(Data) ->
-   Xml = lists:flatten(xmerl:export_simple(Data, xmerl_xml)),
-   list_to_binary(io_lib:format("~s~n", [Xml])).
-
+str(Data) -> lists:flatten(xmerl:export_simple(Data, xmerl_xml)).
 zip(Cx,Data) -> lists:zip(Cx,tl(tuple_to_list(Data))).
 
 cx(Data) -> Cx = record_info(fields, cx), [{cx,zip(Cx,Data),[]}].
 pi(Data) -> Pi = record_info(fields, pi), [{pi,zip(Pi,Data),[]}].
 
-encode(#cx{}=C) -> serialize(cx(C));
-encode(#pi{}=C) -> serialize(pi(C)).
+encode(#cx{}=C) -> str(cx(C));
+encode(#pi{}=C) -> str(pi(C)).
 
-decode(Bin) -> xmerl_scan:string(Bin).
+decode(Bin) ->
+  {#xmlElement{name=N,attributes=L},_} = xmerl_scan:string(Bin),
+  list_to_tuple([N|[J||#xmlAttribute{value=J}<-L]]).
