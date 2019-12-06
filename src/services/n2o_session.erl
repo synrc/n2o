0b637a91129360570610c9c27a3f4aa0595b1038
Namdak Tonpa <maxim@synrc.com>
2019-12-06 05:12:09 +0200

    fix cache and session expirations because of new tuple/3 session storage format

--- src/services/n2o_session.erl
+++ src/services/n2o_session.erl
@@ -3,7 +3,7 @@
 -include_lib("stdlib/include/ms_transform.hrl").
 -description('N2O Session').
 -export([authenticate/2, get_value/3, set_value/3, storage/0, prolongate/0, from/1, ttl/0, till/2]).
--export([clear/1, delete/1, update/1, lookup/1, invalidate_sessions/0]).
+-export([clear/1, delete/1, update/1, lookup/1, invalidate_sessions/0, test/0]).
 
 % PRELUDE
 
@@ -13,7 +13,7 @@ from(X)       -> calendar:gregorian_seconds_to_datetime(X).
 cut(Bin)      -> binary:part(Bin,0,20).
 expired(Till) -> Till < to(calendar:local_time()).
 expire()      -> to(till(calendar:local_time(), ttl())).
-auth(Sid,Exp) -> {{Sid,'auth'},{Exp,[]}}.
+auth(Sid,Exp) -> {{Sid,'auth'},Exp,[]}.
 storage()     -> application:get_env(n2o,session_storage,n2o_session).
 token(A)      -> (storage()):update(A), {'Token',n2o:pickle(A)}.
 token(A,P)    -> (storage()):update(A), {'Token',P}.
@@ -31,27 +31,29 @@ sid(Seed)     -> n2o_secret:sid(Seed).
 authenticate([], Pickle) ->
     case n2o:depickle(Pickle) of
         <<>> -> token(auth(sid(os:timestamp()),expire()));
-        {{Sid,'auth'},{Till,[]}} = Auth ->
+        {{Sid,'auth'},Till,[]} = Auth ->
             case {expired(Till), prolongate()} of
                  {false,false} -> token(Auth,Pickle);
                   {false,true} -> move(Sid), token(auth(Sid,expire()));
                       {true,_} -> (storage()):delete({Sid,auth}),
                                   token(auth(sid(os:timestamp()),expire()))
-            end
+            end;
+         _ -> token(auth(sid(os:timestamp()),expire()))
     end.
 
 get_value(Session, Key, Default) ->
+    io:format("n2o.session: get_value: ~p~n",[{Session, Key, Default}]),
     case (storage()):lookup({Session,Key}) of
          [] -> Default;
-         {{Session,Key},{Till,Value}} -> case expired(Till) of
+         {{Session,Key},Till,Value} -> case expired(Till) of
                 false -> Value;
                 true -> (storage()):delete({Session,Key}), Default end end.
 
 set_value(Session, Key, Value) ->
-    (storage()):update({{Session,Key},{expire(),Value}}), Value.
+    (storage()):update({{Session,Key},expire(),Value}), Value.
 
 move(Sid) ->
-    [ (storage()):update({{Sid,Key},{expire(),Val}}) || {{_,Key},{_,Val}} <- ets:select(cookies,
+    [ (storage()):update({{Sid,Key},expire(),Val}) || {{_,Key},_,Val} <- ets:select(cookies,
         ets:fun2ms(fun(A) when (element(1,element(1,A)) == Sid) -> A end)) ], ok.
 
 clear(Session) ->
@@ -76,9 +78,9 @@ invalidate_sessions() ->
 positive_test() ->
     application:set_env(n2o,nitro_prolongate,false),
     {'Token',B}=n2o_session:authenticate("",""),
-    {{SID,Key},{Till,[]}} = n2o:depickle(B),
+    {{SID,Key},Till,[]} = n2o:depickle(B),
     {'Token',C}=n2o_session:authenticate("",B),
-    {{SID,Key},{Till,[]}} = n2o:depickle(C),
+    {{SID,Key},Till,[]} = n2o:depickle(C),
     delete({SID,'auth'}),
     true=(C==B).
 
@@ -86,14 +88,14 @@ negative_test() ->
     application:set_env(n2o,nitro_prolongate,false),
     application:set_env(n2o, ttl, 2),
     {'Token', TokenA} = n2o_session:authenticate("", ""),
-    {{SID0,_},{_,[]}} = n2o:depickle(TokenA),
+    {{SID0,_},_,[]} = n2o:depickle(TokenA),
     timer:sleep(3000),
     {'Token', TokenB} = n2o_session:authenticate("", TokenA),
-    {{SID1,_},{_,[]}} = n2o:depickle(TokenB),
+    {{SID1,_},_,[]} = n2o:depickle(TokenB),
     application:set_env(n2o, ttl, 60*15),
     TokenWasChanged = TokenA /= TokenB,
     {'Token', TokenC} = n2o_session:authenticate("", TokenB),
-    {{SID2,_},{_,[]}} = n2o:depickle(TokenC),
+    {{SID2,_},_,[]} = n2o:depickle(TokenC),
     NewTokenIsValid = TokenB == TokenC,
     delete({SID0,auth}),
     delete({SID1,auth}),
@@ -108,3 +110,5 @@ test_set_get_value() ->
     ResultValue = get_value(SID, Key, []),
     delete({SID,Key}),
     InputValue == ResultValue.
+
+test() -> [test_set_get_value(),negative_test(),positive_test()].
--- src/ws/n2o_syn.erl
+++ src/ws/n2o_syn.erl
@@ -3,7 +3,7 @@
 -include("n2o.hrl").
 -export(?MESSAGE_API).
 
-init() -> syn:init(), ok.
+init() -> ok.
 send(Pool, Message) -> syn:publish(term_to_binary(Pool),Message).
 reg(Pool) -> reg(Pool,undefined).
 reg(Pool, _Value) ->
@@ -14,5 +14,5 @@ reg(Pool, _Value) ->
 unreg(Pool) ->
     case get({pool,Pool}) of
          undefined -> skip;
-         _Defined -> syn:leave(term_to_binary(Pool), self()), 
+         _Defined -> syn:leave(term_to_binary(Pool), self()),
                      erase({pool,Pool}) end.
