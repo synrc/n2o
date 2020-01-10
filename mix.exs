1df3e96ecf0c120e122b6f11fdcb8ecd1f0f2a06
5HT <maxim@synrc.com>
2020-01-10 16:33:50 +0200

    7.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule N2O.Mixfile do
   def application, do: [mod: {:n2o, []}, applications: []]
   def project do
     [ app: :n2o,
-      version: "7.1.1",
+      version: "7.1.2",
       description: "N2O MQTT TCP WebSocket",
       package: package(),
       deps: deps()]
