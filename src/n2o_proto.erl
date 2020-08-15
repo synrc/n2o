7216a0d64ca61bb74bbbb7dc46e928700d3c0645
Namdak Tonpa <maxim@synrc.com>
2020-08-15 04:31:29 +0300

    7.8.2

--- src/n2o_proto.erl
+++ src/n2o_proto.erl
@@ -1,8 +1,10 @@
 -module(n2o_proto).
--description('N2O WebSocket').
+-description('N2O WebSocket Protocol').
 -include_lib("n2o/include/n2o.hrl").
--export([init/2, finish/2, info/3, stream/3, push/5, init/4, terminate/2, cx/1]).
+-export([init/2, finish/2, info/3, stream/3, push/5, init/4, terminate/2, cx/2]).
 -export([try_info/3, try_info/4]).
+
+% Common protocol for MQTT, TCP and WebSocket
 
 protocols()        -> application:get_env(n2o,protocols,[ n2o_heart ]).
 info(M,R,S)        -> push(M,R,S,protocols(),[]).
@@ -21,12 +23,14 @@ push(M,R,S,[H|T],Acc)     ->
          {reply,M1,R1,S1} -> reply(M1,R1,S1);
                         A -> push(M,R,S,T,[A|Acc]) end.
 
-cx(Req) ->
-  Cookies = cowboy_req:parse_cookies(Req),
+pid(Ctx) when is_tuple(Ctx) -> maps:get(pid, element(4, Ctx)).
+cx(Cookies,Req) ->
   Token = case lists:keyfind(<<"X-Authorization">>, 1, Cookies) of {_,V} -> V; false -> <<>> end,
   Sid = case n2o:depickle(Token) of {{S,_},_} -> S; _ -> <<>> end,
   #cx{actions=[], path=[], req=Req, params=[], session=Sid, token=Token,
       handlers= [ {routes, application:get_env(n2o,routes,?MODULE)} ]}.
+
+% Part of WebSocket Adapter
 
 finish(State, Cx) -> {ok, State, Cx}.
 init(State, Cx)   -> {ok, State, Cx}.
@@ -40,12 +44,12 @@ fold(Fun,Handlers,Ctx) ->
 
 terminate(_,#cx{module=Module}) -> catch Module:event(terminate).
 init(_Transport, Req, _Opts, _) ->
+    Cookies = cowboy_req:parse_cookies(Req),
     {Module,CxInit} = application:get_env(n2o,cx,{n2o_proto,cx}),
-    Zero = Module:CxInit(Req),
+    Zero = Module:CxInit(Cookies,Req),
     put(context,Zero),
     Ctx  = fold(init,Zero#cx.handlers,Zero),
     put(context,Ctx),
-    save_context(Ctx),
     Origin = case cowboy_req:header(<<"origin">>, Req, <<"*">>) of {O,_} -> O; X -> X end,
     ConfigOrigin = iolist_to_binary(application:get_env(n2o,origin,Origin)),
     Req1 = cowboy_req:set_resp_header(<<"Access-Control-Allow-Origin">>, ConfigOrigin, Ctx#cx.req),
@@ -68,11 +72,3 @@ try_info(Module,M,R,S) ->
     catch Err:Rea -> Stack = erlang:get_stacktrace(), ?LOG_EXCEPTION(Err, Rea, Stack), {error,{stack,Stack}} end.
 -endif.
 
-pid(Ctx) when is_tuple(Ctx) -> maps:get(pid, element(4, Ctx)).
-save_context(Ctx) ->
-    case ets:info(web_context) of
-        undefined -> ets:new(web_context, [public, named_table]);
-        _ -> skip
-    end,
-    Pid = pid(Ctx),
-    ets:insert(web_context, {Pid, Ctx}).
