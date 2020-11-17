159f17afb21eece1d04699f8bad2cfa66d33da9f
Andrii Zadorozhnii <doxtop@synrc.com>
2020-11-17 04:09:02 +0200

    export fun

--- src/n2o_proto.erl
+++ src/n2o_proto.erl
@@ -3,6 +3,7 @@
 -include_lib("n2o/include/n2o.hrl").
 -export([init/2, finish/2, info/3, info/4, stream/3, push/5, init/4, terminate/2, cx/2]).
 -export([try_info/4, try_info/5]).
+-export([protocols/0]).
 
 % Common protocol for MQTT, TCP and WebSocket
 
