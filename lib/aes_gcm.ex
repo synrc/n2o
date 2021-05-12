b6a1e01fd18e54356deb44b939431b57db10137e
Namdak Tonpa <maxim@synrc.com>
2021-05-13 01:18:41 +0300

    8.5.0

--- lib/aes_gcm.ex
+++ lib/aes_gcm.ex
@@ -7,7 +7,8 @@ defmodule AES.GCM do
     try do
       cipher = :n2o_secret.unhex(hex)
       <<iv::binary-16, tag::binary-16, bin::binary>> = cipher
-      term = :crypto.crypto_one_time(:aes_gcm, secret_key(), iv, {@aad, bin, tag}, [{:encrypt,false}], [:safe])
+      term = :crypto.crypto_one_time_aead(:aes_256_gcm,
+         secret_key(), iv, bin, @aad, tag, [{:encrypt,false}])
       :erlang.binary_to_term(term, [:safe])
     rescue
       _ -> ""
@@ -19,7 +20,8 @@ defmodule AES.GCM do
     iv = :crypto.strong_rand_bytes(16)
 
     {cipher, tag} =
-      :crypto.crypto_one_time(:aes_gcm, secret_key(), iv, {@aad, bin}, [{:encrypt,true}])
+      :crypto.crypto_one_time_aead(:aes_256_gcm,
+         secret_key(), iv, bin, @aad, [{:encrypt,true}])
 
     bin = iv <> tag <> cipher
     :n2o_secret.hex(bin)
